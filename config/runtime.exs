5aef36088f78ce271bbe4a518e6770965db1e68f
joeybellerose <joey.bellerose@candoranalytics.io>
2022-12-26 16:21:19 -0500

    changed port

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "elixir4ds-production.up.railway.app"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :elixir4ds, Elixir4dsWeb.Endpoint,
